d82cb6891ccf4d2182cff400dcec96a2a0d19cad
Luke Granger-Brown <git@lukegb.com>
2021-01-08 01:58:22 +0000

    pomerium: init at 0.11.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17591,6 +17591,8 @@ in
   };
   pflogsumm = callPackage ../servers/mail/postfix/pflogsumm.nix { };
 
+  pomerium = callPackage ../servers/http/pomerium { };
+
   postgrey = callPackage ../servers/mail/postgrey { };
 
   pshs = callPackage ../servers/http/pshs { };

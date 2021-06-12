5d87f1a207ab917d8e09a7752045303951908b12
Luke Granger-Brown <git@lukegb.com>
2021-06-12 19:31:06 +0000

    firefox: reenable p11-kit support in NSS
    We're now building p11-kit with a reasonable setup for use with NSS.

--- pkgs/applications/networking/browsers/firefox/common.nix
+++ pkgs/applications/networking/browsers/firefox/common.nix
@@ -118,7 +118,7 @@ let
 
   # Disable p11-kit support in nss until our cacert packages has caught up exposing CKA_NSS_MOZILLA_CA_POLICY
   # https://github.com/NixOS/nixpkgs/issues/126065
-  nss_pkg = if lib.versionOlder ffversion "83" then nss_3_53 else nss.override { useP11kit = false; };
+  nss_pkg = if lib.versionOlder ffversion "83" then nss_3_53 else nss;
 
   # --enable-release adds -ffunction-sections & LTO that require a big amount of
   # RAM and the 32-bit memory space cannot handle that linking

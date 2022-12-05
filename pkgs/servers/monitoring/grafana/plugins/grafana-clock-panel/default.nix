a5bee190c123e023f370768075ff3179463fffdb
Luke Granger-Brown <git@lukegb.com>
2022-12-05 10:20:48 +0000

    grafanaPlugins.grafana-clock-panel: 1.1.3 → 2.1.1

--- pkgs/servers/monitoring/grafana/plugins/grafana-clock-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-clock-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin rec {
   pname = "grafana-clock-panel";
-  version = "1.1.3";
-  zipHash = "sha256-80JaMhY/EduSWvFrScfua99DGhT/FJUqY/kl0CafKCs=";
+  version = "2.1.1";
+  zipHash = "sha256-cDwKObAxtyY8Ng6UQcYMfHk8vh3XvoJv5ojc2BEhxdQ=";
   meta = with lib; {
     description = "Clock panel for Grafana";
     license = licenses.mit;

f057f5f74356897f9b3f5ead9d1d10d45d31baa2
Luke Granger-Brown <git@lukegb.com>
2023-02-07 10:19:15 +0000

    grafanaPlugins.grafana-worldmap-panel: 0.3.3 → 1.0.2

--- pkgs/servers/monitoring/grafana/plugins/grafana-worldmap-panel/default.nix
+++ pkgs/servers/monitoring/grafana/plugins/grafana-worldmap-panel/default.nix
@@ -2,8 +2,8 @@
 
 grafanaPlugin rec {
   pname = "grafana-worldmap-panel";
-  version = "0.3.3";
-  zipHash = "sha256-3n1p3SvcBQMmnbnHimLBP7hauVV1IS3SMwttUWTNvb8=";
+  version = "1.0.2";
+  zipHash = "sha256-STD09fKGH4ObLPsfGmC6eZIISEKzMaLfNjRLEBKBqhY=";
   meta = with lib; {
     description = "World Map panel for Grafana";
     license = licenses.mit;
